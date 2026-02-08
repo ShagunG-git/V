db444cf3bbd45937bac4764de17caa8cb0a31b78
shagun <shagun@rdpl.com>
2026-02-08 19:33:17 +0530

    k

--- script.js
+++ script.js
@@ -13,32 +13,57 @@ const noTexts = [
 ];
 
 const images = [
-    "images/image2.jpg",
-    "images/image3.jpg",
-    "images/image4.jpg",
-    "images/image4.jpg"
+    "images/image2.jpeg",
+    "images/image3.jpeg",
+    "images/image4.jpeg",
+    "images/image4.jpeg"
 ];
 
 noBtn.addEventListener("click", () => {
-    if (noCount < noTexts.length) {
-        text.innerText = noTexts[noCount];
-        image.src = images[noCount];
-        noCount++;
-    }
+    text.innerText = noTexts[noCount];
+    image.src = images[noCount];
+
+    noCount++;
+
+    yesBtn.style.transform = `scale(${1 + noCount * 0.3})`;
 
     if (noCount === noTexts.length) {
         noBtn.style.display = "none";
         yesBtn.style.width = "100%";
-        yesBtn.style.fontSize = "24px";
     }
 });
 
 yesBtn.addEventListener("click", () => {
     text.innerText = "Ooo my mutki shona bubu 😘";
-    image.src = "images/image5.jpg";
     noBtn.style.display = "none";
+
+    image.src = "images/image5.jpeg";
+    image.style.filter = "none";
+
+    document.querySelector(".card").classList.add("hooray");
+
+    startConfetti();
 
     setTimeout(() => {
         text.innerText = "Mujhse bhi to puch le moti kahi ki 😜";
     }, 10000);
 });
+
+
+
+function startConfetti() {
+    const confetti = document.getElementById("confetti");
+    confetti.innerHTML = "";
+
+    for (let i = 0; i < 80; i++) {
+        const piece = document.createElement("div");
+        piece.classList.add("confetti-piece");
+        piece.style.left = Math.random() * 100 + "vw";
+        piece.style.background =
+            ["#ff4d6d", "#ffd166", "#06d6a0", "#4dabf7"][
+            Math.floor(Math.random() * 4)
+            ];
+        piece.style.animationDuration = 2 + Math.random() * 3 + "s";
+        confetti.appendChild(piece);
+    }
+}
